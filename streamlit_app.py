ef02806cf01a22185b648f6db3652d1a28b5bb13
kritika29joshi <kritikjoshi@gmail.com>
2025-01-19 08:48:00 -0600

    Update streamlit_app.py
    🥋 Add the New SEARCH_ON Column to the Dataframe that feeds the Multiselect
    pt2

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
 
-st.title('My Parents New Healthy Diner')
+st.title(':cup_with_straw: Customize Your Smoothie! :cup_with_straw:')
 st.write('Choose the fruits you want in your custom Smoothie!')
 
 
@@ -11,7 +11,7 @@ st.write('The name on your Smoothie will be:', name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table('Smoothies.public.fruit_options').select(col('fruit_name')).collect()
+my_dataframe = session.table('Smoothies.public.fruit_options').select(col('fruit_name')).(col('search_on'))
 st.dataframe(data = my_dataframe, use_container_width=True)
 st.stop()
 
